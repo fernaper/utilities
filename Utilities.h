9495c111de4b71ec32d1a67ff76094feeafce1a4
TheCorPlay <thecorplay@gmail.com>
2016-04-12 23:50:54 +0200

    v0.0v6-alpha
    New version with new methods.

--- Utilities.h
+++ Utilities.h
@@ -4,7 +4,7 @@
  *
  *	By: TheCorPlay
  *	E-Mail: thecorplay@gmail.com
- *	Current version: v0.0.5
+ *	Current version: v0.0.6
  *	______________________________
  *	
  *	COPYRIGHT ©
@@ -32,9 +32,40 @@
 #define _UTILITIES_H
 
 #include <string>
-#include <iostream>
+//#include <iostream>
+
+/// Given a number return its square root.
+/// O = n;
+double u_sqrt (int number)
+{
+	double radical = number, total = 0;
+    while (total != radical)
+    {
+        total = radical;
+        radical = (number/radical+radical)/2;
+    }
+	
+	return radical;
+}
+
+/// Returns if a given integer number its prime or not.
+/// O = n; // n = log(n)
+bool u_prime (int p) {
+	if (p < 0)
+		p = -p;
+	
+	if(p<2)
+		return false; 
+		
+	for(int i=2;i<=u_sqrt(p);i++)
+		if(p%i == 0)
+			return false; 
+			
+	return true;
+}
 
 /// Given two integers (the base and the exponent) returns a to the power of b.
+/// O = n;  // n = b
 long int u_exponents(int a, int b)
 {
 	if (b <= 0)
@@ -44,6 +75,7 @@ long int u_exponents(int a, int b)
 }
 
 /// Given a string returns an integer ignoring any other character. (CSTOI = Controlled string to int)
+/// O = n; // n = string.length()
 int u_cstoi (const std::string &text)
 {
 	unsigned int i = 0;
@@ -67,6 +99,7 @@ int u_cstoi (const std::string &text)
 }
 
 /// Given a string returns an integer. (STOI = String to int)
+/// O = n; // n = string.length ()
 int u_stoi (const std::string &text)
 {
 	unsigned int i;
@@ -86,6 +119,7 @@ int u_stoi (const std::string &text)
 }
 
 /// Given a string returns an unsigned integer ignoring any other character. (STUI = Controlled string to unsigned int)
+/// O = n; // n = string.length()
 unsigned int u_stui (const std::string &text)
 {
 	unsigned int i = 0, sol = 0;
@@ -97,7 +131,9 @@ unsigned int u_stui (const std::string &text)
 	return sol;
 }
 
-/// Given a string and a char (that char means with what symbol start the decimal part) returns the equivalents double. (STOD = Controlled string to double)
+/// Given a string and a char (that char means with what symbol start the decimal part)
+/// returns the equivalents double. (STOD = Controlled string to double)
+/// O = n; // n = string.length()
 double u_stod (const std::string &text, const char decimal)
 {
 	unsigned int i = 0, iaux;
@@ -132,6 +168,7 @@ double u_stod (const std::string &text, const char decimal)
 }
 
 /// Given an integer, return the equivalent string. (ITOSTR = Integer to string)
+/// O = n^2;
 std::string u_itostr (int number)
 {
 	std::string sol = "";
@@ -155,6 +192,7 @@ std::string u_itostr (int number)
 }
 
 /// Given a string and a separator, erase from the string all the aparitions of the separator.
+/// O = n^2;
 void u_eraseAll (std::string &text, const char separator)
 {
 	for (unsigned int i = 0; i < text.length(); i++)
@@ -162,6 +200,7 @@ void u_eraseAll (std::string &text, const char separator)
 }
 
 /// Given a text and a character, returns the number of times that the character appears. (COUNTC = Count Characters)
+/// O = n; // n = string.length()
 unsigned int u_countChar (const std::string &text, const char separator)
 {
 	unsigned int count = 0;
@@ -174,6 +213,7 @@ unsigned int u_countChar (const std::string &text, const char separator)
 
 /// Given a text and a separator, returns the number of words separeted by the separator. (WORDS = Number of Words)
 /// If comes an string with only the separator it will return 2 (becouse exist two empty sides)
+/// O = n; // n = string.length()
 unsigned int u_words (const std::string &text, const char separator)
 {
 	unsigned int num = 0, i = 0;
@@ -190,6 +230,7 @@ unsigned int u_words (const std::string &text, const char separator)
 
 /// Given an empty array of strings, a text, the maximum number of words, and the separator, it will give ou an array with one word in each position.
 /// Also it will return the number of words that he use.
+/// O = 3n^2 = n^2;
 unsigned int u_split (std::string array[], std::string text, const unsigned int words, const char separator)
 {
 	unsigned int count = 0;
@@ -207,6 +248,7 @@ unsigned int u_split (std::string array[], std::string text, const unsigned int 
 
 /// Given an empty array of strings, a text, and the separator, it will give you an array with one word in each position.
 /// Also it will return the number of words that he use.
+/// O = 3n^2 = n^2;
 unsigned int u_split (std::string array[], std::string text, const char separator)
 {
 	unsigned int count = 0;
@@ -225,6 +267,7 @@ unsigned int u_split (std::string array[], std::string text, const char separato
 
 /// Given an empty array of strings, a text, it will give you an array with one word in each position separated by ' '.
 /// Also it will return the number of words that he use.
+/// O = 3n^2 = n^2;
 unsigned int u_split (std::string array[], std::string text)
 {
 	return u_split (array, text, ' ');
@@ -232,6 +275,7 @@ unsigned int u_split (std::string array[], std::string text)
 
 /// Given an array of strings, the number of words of the array and a separator.
 /// It will return a string with all the array concatenated and separated by the separator.
+/// O = n^2;
 std::string u_dsplit (std::string array[], unsigned int count, const char separator)
 {
 	std::string text = array[0];
@@ -244,6 +288,7 @@ std::string u_dsplit (std::string array[], unsigned int count, const char separa
 
 /// Given an array of strings and the number of words of the array.
 /// It will return a string with all the array concatenated and separated by the default separator ' '.
+/// O = n^2;
 std::string u_dsplit (std::string array[], unsigned int count)
 {
 	return u_dsplit (array, count, ' ');
@@ -252,6 +297,7 @@ std::string u_dsplit (std::string array[], unsigned int count)
 /// Given an array of any type, something that you are searching on the array (Binary Search), the space who we search and an empty postion.
 /// The variable pos returns the position where it is or should be if not found.
 /// The method returns if you found it or not
+/// O = log(n);
 template <class TYPE>
 bool u_search(TYPE list[], TYPE search, int count, int &pos) {
   int start = 0, end = count - 1, half;
@@ -275,7 +321,7 @@ bool u_search(TYPE list[], TYPE search, int count, int &pos) {
 	return found;
 }
 
-int main ()
+/*int main ()
 {
 	// Problem 1: Obtain the addition of all values separated by a specific symbol (not the others).
 	{
@@ -325,8 +371,26 @@ int main ()
 		u_eraseAll(text, '_');
 		std::cout << "Problem 4: " << text << "\n";
 	}
+	// Problem 5: Given a string, split into strings, parse to numbers and if its no prime show the Square Root of its number
+	{
+		std::string text = "1, 2, 3, 4, 5, 6, 7, 8, 9, 10"; // Primes: 2, 3, 5, 7
+		unsigned int words = u_words (text, ' ');
+		std::string array [words];
+		
+		u_split (array,text,' ');
+		
+		std::cout << "Problem 4: " << "\n";
+		
+		for (unsigned int i = 0; i < words; i++) {
+			int number = u_cstoi(array[i]);
+			if (!u_prime(number))
+				std::cout << "	   sqrt(" << number << ") = " << u_sqrt(number) << "\n";
+		}
+		
+		std::cout << "\n";
+	}
 	
 	return 0;
-}
+}*/
 
 #endif
